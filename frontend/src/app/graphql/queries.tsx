7d8c059dc2621c585da2f1017d125a4a0a8aecb1
Martin Johnsson <maartinjohnsson@gmail.com>
2024-01-23 20:40:09 +0100

    Update client graphql client to function in NextJS 13+

--- frontend/src/app/graphql/queries.tsx
+++ frontend/src/app/graphql/queries.tsx
@@ -1,33 +1,15 @@
 import { gql } from 'graphql-tag';
 
-export const LIST_BLOG = gql(`query {
-  blogPosts {
-    data {
-      id
-      attributes {
-        title
+export const BLOG_POSTS = gql`
+  query {
+    blogPosts {
+      data {
+        id
+        attributes {
+          title
+          body
+        }
       }
     }
   }
-}`);
-
-export const SINGLE_BLOG = gql(`query ($blogId: string!) {
-  blogPosts(filters: {id: {eq: $blogId}}) {
-    data {
-      id
-      attributes {
-        title
-        description
-        body
-      }
-    }
-  }
-}`);
-
-export const LIST_ID = gql(`query {
-  blogPosts {
-    data {
-      id
-    }
-  }
-}`);
+`;
