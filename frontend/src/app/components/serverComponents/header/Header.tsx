624db0a21118246741f69fb7d42eafcd0a67dff3
Martin Johnsson <maartinjohnsson@gmail.com>
2024-01-30 16:25:58 +0100

    Update header to flow over main

--- frontend/src/app/components/serverComponents/header/Header.tsx
+++ frontend/src/app/components/serverComponents/header/Header.tsx
@@ -1,10 +1,11 @@
 'use server';
+
 import React from 'react';
 import SearchButton from '../../clientComponents/SearchButton';
 
 const Header = () => {
   return (
-    <header className='border border-dotted border-b-white-700 border-t-0 border-r-0 border-l-0 h-20 w-full flex flex-col justify-align-center items-center justify-items-center h-100 justify-center fixed z-1'>
+    <header className='border border-dotted border-b-white-700 border-t-0 border-r-0 border-l-0 w-full flex flex-col justify-align-center items-center justify-items-center justify-center fixed z-1 top-0 h-24'>
       <div className='flex flex-row w-100 h-5 gap-5'>
         <SearchButton />
         <div>Personal Blog</div>
