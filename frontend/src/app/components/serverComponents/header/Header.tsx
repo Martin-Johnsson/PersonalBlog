a6f8ba40ae0ec4d25fdacff3e847c1679ae88da3
Martin Johnsson <maartinjohnsson@gmail.com>
2024-01-30 16:30:52 +0100

    Update header to use semantic HTML tags

--- frontend/src/app/components/serverComponents/header/Header.tsx
+++ frontend/src/app/components/serverComponents/header/Header.tsx
@@ -6,10 +6,10 @@ import SearchButton from '../../clientComponents/SearchButton';
 const Header = () => {
   return (
     <header className='border border-dotted border-b-white-700 border-t-0 border-r-0 border-l-0 w-full flex flex-col justify-align-center items-center justify-items-center justify-center fixed z-1 top-0 h-24'>
-      <div className='flex flex-row w-100 h-5 gap-5'>
+      <nav className='flex flex-row w-100 h-5 gap-5'>
         <SearchButton />
-        <div>Personal Blog</div>
-      </div>
+        <h1>Personal Blog</h1>
+      </nav>
     </header>
   );
 };
