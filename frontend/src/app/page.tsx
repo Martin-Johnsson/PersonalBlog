262833d9d4bd06c7fc87364c6a6e32baf63e8305
Martin Johnsson <maartinjohnsson@gmail.com>
2024-03-19 19:37:16 +0100

    Add fetch method for posts in src/app/page.tsx

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,15 +1,15 @@
 'use server';
 
+const postsURL: string = process.env.POSTS_URL ?? '';
+
 import PostList from './components/serverComponents/post/PostList';
 
 export default async function Page() {
-  const fetchPosts = await fetch('http://localhost:1337/api/blog-postss').then(
-    (response: Response) => {
-      if (!response.ok) {
-        throw new Error(`Error fetching posts (Status: ${response.status})`);
-      } else return response.json();
-    }
-  );
+  const fetchPosts = await fetch(postsURL).then((response: Response) => {
+    if (!response.ok) {
+      throw new Error(`Error fetching posts (Status: ${response.status})`);
+    } else return response.json();
+  });
 
   return (
     <div>
